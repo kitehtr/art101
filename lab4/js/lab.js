aa06bab00088f691690fbc6172556979c65cc251
Keith Tran <idontknowaboutthis5@gmail.com>
2024-10-17 20:31:52 -0700

    adding setup for lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// title of game
+// Implementation of game
+// Author: Keith Tran & Josh Chow
+// Date: 10/17/24
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Pseudocode:

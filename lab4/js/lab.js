612342beb860144a6aa63d6f65931192c48d7d2f
Keith Tran <idontknowaboutthis5@gmail.com>
2024-10-17 21:09:41 -0700

    finishing lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -3,4 +3,12 @@
 // Author: Keith Tran & Josh Chow
 // Date: 10/17/24
 
-//Pseudocode:
+//Pseudocode for Chess:
+// Generate Board
+// Generate chess pieces PLAYER1
+// Generate chess pieces PLAYER2
+// Place chess pieces 
+// Determine if the chess pieces are allowed on that space
+// Eliminate chess pieces when crossed by either player
+// Create a counter of how many chess pieces lost
+// Then determine winner

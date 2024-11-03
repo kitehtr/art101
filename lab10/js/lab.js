814fb279ed68badc2f8c3d8421b6f119315be660
Keith Tran <idontknowaboutthis5@gmail.com>
2024-11-02 18:03:51 -0700

    updating links

---
+++ lab10/js/lab.js
@@ -0,0 +1,34 @@
+// lab.js - Lab 8: Anon Functions and Callbacks
+// Author: Keith Tran & Josh Chow
+// Date: 10/30/24
+
+array = [1,10,2300203,20,420,21,23,27];
+
+function addTwoNums (x) {
+    var results = x + x;
+    return results;
+}
+
+// test function
+console.log("What is 10 + 10? ", addTwoNums(10));
+console.log("What is 420 + 420? ", addTwoNums(420));
+console.log("What is 21 + 21? ", addTwoNums(21));
+
+console.log("My array", array);
+
+var result = array.map(addTwoNums);
+console.log("Double every element in the array: ", result);
+
+var mapResults = array.map(function(x) {
+    return x * x;
+})
+
+console.log("Square of array: ", mapResults);
+
+// Use jQuery to select the element by its ID and set the HTML content
+$("#output").html("Original Array: [" + array.join(", ") + "]<br><br>");
+$("#output").append("Squaring of each element in the array: [" + mapResults.join(", ") + "]<br><br>");
+$("#output").append("Double of each element in the array: [" + result.join(", ") + "]<br><br>");
+
+
+

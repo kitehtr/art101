d4bfd17d494076d966e9a76b55cad48bc5a551fc
Keith Tran <idontknowaboutthis5@gmail.com>
2024-11-02 18:36:26 -0700

    updating lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,34 +1,40 @@
-// lab.js - Lab 8: Anon Functions and Callbacks
-// Author: Keith Tran & Josh Chow
-// Date: 10/30/24
-
-array = [1,10,2300203,20,420,21,23,27];
-
-function addTwoNums (x) {
-    var results = x + x;
-    return results;
-}
-
-// test function
-console.log("What is 10 + 10? ", addTwoNums(10));
-console.log("What is 420 + 420? ", addTwoNums(420));
-console.log("What is 21 + 21? ", addTwoNums(21));
-
-console.log("My array", array);
-
-var result = array.map(addTwoNums);
-console.log("Double every element in the array: ", result);
-
-var mapResults = array.map(function(x) {
-    return x * x;
-})
-
-console.log("Square of array: ", mapResults);
+/*
+    lab.js - Lab 9: Libraries & jQuery
+    Requirements: jQuery must be loaded for this script to work.
+    Authors: Keith Tran & Josh Chow
+    Date: 11/2/24
+*/
 
 // Use jQuery to select the element by its ID and set the HTML content
-$("#output").html("Original Array: [" + array.join(", ") + "]<br><br>");
-$("#output").append("Squaring of each element in the array: [" + mapResults.join(", ") + "]<br><br>");
-$("#output").append("Double of each element in the array: [" + result.join(", ") + "]<br><br>");
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>")
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
 
+// add button to problems section
+$("#problems").append("<button id='button-problems'>Make Special</button>")
+// add a click listener to the challenge button
+$("#button-problems").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+});
 
+// add button to Reflection section
+$("#reflection").append("<button id='button-reflection'>Make Special</button>")
+// add a click listener to the challenge button
+$("#button-reflection").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#reflection").toggleClass("special");
+});
+
+// add button to problems section
+$("#results").append("<button id='button-results'>Make Special</button>")
+// add a click listener to the challenge button
+$("#button-results").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
+});
 

dedcf348885b9052ff29ea55a0ae1f20bae7dfeb
Keith Tran <idontknowaboutthis5@gmail.com>
2024-11-18 20:59:38 -0800

    completing extra credit

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,35 +1,70 @@
 /*
     lab.js - Lab 13: Loops
-    Requirements: Working with your partner, experiment with loops.
+    Requirements: Add configurable max number and factors for FizzBuzz.
     Authors: Keith Tran & Josh Chow
     Date: 11/18/24
 */
 
+const DEFAULT_MAX = 100;
+const DEFAULT_FACTORS = [
+    { factor: 3, text: "Fizz" },
+    { factor: 4, text: "Buzz" },
+    { factor: 5, text: "Boom" },
+    { factor: 7, text: "Bang" }
+];
+
 function FizzBuzz() {
-    let oneLongString = ""; 
+    const max = parseInt($("#maxNumber").val()) || DEFAULT_MAX;
 
-    for (let i = 1; i <= 200; i++) {
-        let str = i + " "; 
-        let words = ""; 
+    const factors = [];
+    $(".factor-row").each(function () {
+        const factor = parseInt($(this).find(".factor").val());
+        const text = $(this).find(".text").val();
+        if (factor && text) {
+            factors.push({ factor, text });
+        }
+    });
 
-        if (i % 3 === 0) {
-            words += "Fizz";
-        }
-        if (i % 5 === 0) {
-            words += "Buzz";
-        }
-        if (i % 7 === 0) {
-            words += "Boom";
-        }
+    let outputHTML = "<div class='output-columns'>";
+    for (let i = 1; i <= max; i++) {
+        let str = i + " - ";
+        let words = "";
+
+        factors.forEach(({ factor, text }) => {
+            if (i % factor === 0) {
+                words += text;
+            }
+        });
 
         if (words) {
             str += words + "!";
         }
 
-        oneLongString += str + "<br>";
+        outputHTML += `<div>${str}</div>`;
     }
+    outputHTML += "</div>";
 
-    $("#output").html(oneLongString);
+    $("#output").html(outputHTML);
 }
 
-FizzBuzz();
+function addFactorRow() {
+    $("#factors").append(`
+        <div class="factor-row">
+            <input type="number" class="factor" placeholder="Factor (e.g., 3)" />
+            <input type="text" class="text" placeholder="Text (e.g., Fizz)" />
+        </div>
+    `);
+}
+
+$(document).ready(function () {
+    $("#maxNumber").val(DEFAULT_MAX);
+    DEFAULT_FACTORS.forEach(({ factor, text }) => {
+        addFactorRow();
+        const lastRow = $(".factor-row").last();
+        lastRow.find(".factor").val(factor);
+        lastRow.find(".text").val(text);
+    });
+
+    $("#runButton").click(FizzBuzz);
+    $("#addFactorButton").click(addFactorRow);
+});
